28168231f117a17b59a16dda2105b4fcee324fc4
RobertIskandaraniCodigo <roberto.iskandarani@codigo.ai>
2025-05-07 21:24:12 -0300

    Test: adding a test for delete many meetings

--- Server/test/test.js
+++ Server/test/test.js
@@ -89,4 +89,53 @@ describe('Meeting API', function () {
         });
     });
   });
+
+  describe('/DELETE Multiple Meetings', function () {
+    xit('Delete multiple meetings', function (done) {
+      // Create first meeting
+      Meeting.create({
+        agenda: 'Test Meeting 1',
+        attendes: [],
+        attendesLead: [],
+        location: 'Test Location 1',
+        related: 'Contact',
+        dateTime: new Date(),
+        notes: 'Test Notes 1',
+        createBy: '507f1f77bcf86cd799439011',
+      })
+        .then(function (meeting1) {
+          // Create second meeting
+          return Meeting.create({
+            agenda: 'Test Meeting 2',
+            attendes: [],
+            attendesLead: [],
+            location: 'Test Location 2',
+            related: 'Lead',
+            dateTime: new Date(),
+            notes: 'Test Notes 2',
+            createBy: '507f1f77bcf86cd799439011',
+          }).then(function (meeting2) {
+            return [meeting1, meeting2];
+          });
+        })
+        .then(function (meetings) {
+          const meetingIds = meetings.map((meeting) => meeting._id);
+          return chai
+            .request(app)
+            .delete('/api/meeting/deleteMany')
+            .set('Authorization', testToken)
+            .send({ meetingIds });
+        })
+        .then(function (res) {
+          res.should.have.status(200);
+          res.body.should.have.property('message').eql('done');
+          res.body.should.have.property('updatedMeetings');
+          res.body.updatedMeetings.should.be.an('array');
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+  });
 });
